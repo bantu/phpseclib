0d33730c12db15bfb9934e68bc487ce1c0f1ad3b
Andreas Fischer <bantu@phpbb.com>
2012-06-05 17:27:15 +0200

    [feature/phpunit] Some directory handling tests.

--- tests/Net/SFTPTest.php
+++ tests/Net/SFTPTest.php
@@ -9,6 +9,7 @@ class Net_SFTPTest extends Net_TestBase
 {
 	const EXAMPLE_FILENAME = 'example.txt';
 	const EXAMPLE_CONTENT = "The quick brown fox jumps over the lazy dog\n";
+	const EXAMPLE_DIRNAME = 'phpseclib_dir_test';
 
 	public function testConstructor()
 	{
@@ -107,6 +108,98 @@ class Net_SFTPTest extends Net_TestBase
 			isset($result[self::EXAMPLE_FILENAME]['size']) &&
 			$result[self::EXAMPLE_FILENAME]['size'] === strlen(self::EXAMPLE_CONTENT),
 			'Failed asserting that rawlist() result contains the example file and its size is equal to the excepted size.'
+		);
+	}
+
+	/**
+	* @depends testRSAPrivateKeyLogin
+	*/
+	public function testMkDir($sftp)
+	{
+		$dirname = self::EXAMPLE_DIRNAME;
+
+		$this->assertTrue(
+			$sftp->mkdir($dirname),
+			"Failed asserting that a new directory called '$dirname' could be created."
+		);
+
+		$this->assertFalse(
+			$sftp->mkdir($dirname),
+			"Failed asserting that a new directory called '$dirname' could not be created (because it already exists)."
+		);
+
+		return $sftp;
+	}
+
+	/**
+	* @depends testMkDir
+	*/
+	public function testChDir($sftp)
+	{
+		$dirname = self::EXAMPLE_DIRNAME;
+		$pwd = $sftp->pwd();
+
+		$this->assertTrue(
+			$sftp->chdir($dirname),
+			"Failed asserting that directory could be changed to '$dirname'."
+		);
+
+		$this->assertEquals(
+			$pwd . $dirname . '/',
+			$sftp->pwd(),
+			'Failed asserting that result of pwd() returns the new path.'
+		);
+
+		$this->assertTrue(
+			$sftp->chdir('../'),
+			"Failed asserting that directory could be changed to one level up."
+		);
+
+		return $sftp;
+	}
+
+	/**
+	* @depends testChDir
+	*/
+	public function testRenameDir($sftp)
+	{
+		$dirname = self::EXAMPLE_DIRNAME;
+		$dirname2 = self::EXAMPLE_DIRNAME . '2';
+		$dirname3 = self::EXAMPLE_DIRNAME . '3';
+
+		$this->assertTrue(
+			$sftp->rename($dirname, $dirname2),
+			"Failed asserting that directory '$dirname' could be renamed to '$dirname2'."
+		);
+
+		$this->assertFalse(
+			$sftp->rename($dirname, $dirname3),
+			"Failed asserting that directory '$dirname' could not be renamed to '$dirname3' (because '$dirname' does not exists)."
+		);
+
+		$this->assertTrue(
+			$sftp->rename($dirname2, $dirname),
+			"Failed asserting that directory '$dirname2' could be renamed back to '$dirname'."
+		);
+
+		return $sftp;
+	}
+
+	/**
+	* @depends testRenameDir
+	*/
+	public function testRmDir($sftp)
+	{
+		$dirname = self::EXAMPLE_DIRNAME;
+
+		$this->assertTrue(
+			$sftp->rmdir($dirname),
+			"Failed asserting that the '$dirname' directory could be deleted."
+		);
+
+		$this->assertFalse(
+			$sftp->rmdir($dirname),
+			"Failed asserting that the '$dirname' could not be deleted (because it does not exists)."
 		);
 	}
 }
